b07a81f8c542f547a2985d967231dfa3fa850dc1
jdalksd <adidpopovandro@gmail.com>
2025-05-05 11:20:10 +0300

    Add files via upload

---
+++ indext.js
@@ -0,0 +1,20 @@
+{
+
+    
+    let btn_submit=document.getElementById("btn_sumbit")
+    if (condition) {
+        btn_submit. onclick = function btn_submit() {
+            btn_submit(this.onclick);
+            console .log(" sumbmit")  
+    }
+     }
+    else{
+btn_submit =  function btn_submit () {
+            btn_cancel (this.not_submit);
+            console.log ( "not sumbmit"); 
+    }
+        
+    }
+    
+}
+
---
+++ main.js
@@ -0,0 +1,24 @@
+{
+
+
+
+ let btn_login= document.getElementById("btn_login");
+ let btn_cancel= document.getElementById("btn_cancel");
+ if (btn_login) {
+   btn_login. onclick = function btn_login() {
+    btn_loginlog(this.onclick);
+    console .log(" A FOST  LOGAT")
+   }
+  
+  
+ }
+ else{
+btn_cancel =  function btn_cancel () {
+ btn_cancel (this.null);
+ console.log ( "nu a  fost logat");
+}
+
+
+ }
+
+ }
